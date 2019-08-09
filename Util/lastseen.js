7a8a74e7b896e35955cd8de1c213313a6cc44c38
Joe <52585150+citizenofcode@users.noreply.github.com>
2019-08-09 12:30:05 -0700

    Inactivity warning

--- Util/lastseen.js
+++ Util/lastseen.js
@@ -1,8 +1,10 @@
 const { RichEmbed } = require('discord.js');
+const ms = require('ms');
 const moment = require('moment');
 require('moment-duration-format');
 
 module.exports = async (client, user) => {
+	const warnTime = ms(client.config.inactivityWarning) || null;
 	let lastseen = 'Unknown (Not enough information gathered)';
 	const embed = new RichEmbed()
 		.setColor(client.config.color)
@@ -12,8 +14,15 @@ module.exports = async (client, user) => {
 	if (onlineStatus.includes(user.presence.status)) { lastseen = 'Right now'; }
 	else {
 		lastseen = await client.db.get(user.id);
-		if (lastseen) lastseen = `${moment.duration(Date.now() - lastseen).format('D [days], H [hourss], m [minutes] [and] s [seconds]')} ago`;
-		else lastseen = 'Unknown (Not enough information gathered)';
+		if (lastseen) {
+			lastseen = `${moment.duration(Date.now() - lastseen).format('D [days], H [hours], m [minutes] [and] s [seconds]')} ago`;
+			if (!warnTime) return;
+			if ((Date.now() - (await client.db.get(user.id))) > warnTime) {
+				embed.addField('⚠ Inactivity Warning', `This user has not been online for more then ${ms(warnTime, { long: true })}!`);
+				embed.setColor(0xed2939);
+			}
+		}
+		else { lastseen = 'Unknown (Not enough information gathered)'; }
 	}
 	embed.setDescription(lastseen);
 	return embed;
