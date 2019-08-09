ab9d94ae909ea1d465ce41fc0f7c91a382defb33
Joe <52585150+citizenofcode@users.noreply.github.com>
2019-08-09 11:44:25 -0700

    Update presenceUpdate.js

--- Events/presenceUpdate.js
+++ Events/presenceUpdate.js
@@ -1,5 +1,6 @@
 module.exports = async (client, oldMember, newMember) => {
-	if (newMember.user.presence.status === 'offline') {
+	if (oldMember.presence.status === newMember.presence.status) return;
+	if (newMember.presence.status === 'offline') {
 		await client.db.set(newMember.user.id, Date.now());
 		return;
 	}
